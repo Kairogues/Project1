775e8e1af790a019ac6be507e6bf9baf861ba67f
Huy Pham <huy.phamkeawy@hcmut.edu.vn>
2026-08-10 23:05:38 +0700

    Implement Enemy base class

--- Assets/Entities/Enemy/Enemy.cs
+++ Assets/Entities/Enemy/Enemy.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region EnemyName
+    [SerializeField] private string enemyName;
+    public string EnemyName
     {
-        
+        set 
+        { 
+            enemyName = value; 
+            name = enemyName;
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        if (!string.IsNullOrEmpty(enemyName))
+        {
+            name = enemyName;
+        }
+    }
+    #endregion
+
+    [SerializeField] private IObjectPool<GameObject> objectPool;
+
+    public void SetPool(IObjectPool<GameObject> objectPool)
+    {
+        this.objectPool = objectPool;
+    }
+
+    public void ReleaseToPool()
+    {
+        objectPool.Release(gameObject);
     }
 }
